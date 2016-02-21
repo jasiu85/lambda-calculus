114ebfa4c1eec7c01d75fa296335beb50e220a79
Michal Jaszczyk <jasiu85@gmail.com>
2016-02-21 03:35:45 +0000

    Fix bugs and typos.

--- Lambda/Named.hs
+++ Lambda/Named.hs
@@ -14,9 +14,9 @@ to_named fv_names bv_names t =
   to_named' 0 t
   where
     to_named' lnl (TermVar k) =
-      NamedTermVar (var_name lnl k)
+      NamedTermVar (v_name lnl k)
     to_named' lnl (TermLambda t) =
-      NamedTermLambda (to_named' (lnl + 1) t)
+      NamedTermLambda (bv_names !! lnl) (to_named' (lnl + 1) t)
     to_named' lnl (TermApply tf tx) =
       NamedTermApply (to_named' lnl tf) (to_named' lnl tx)
     v_name lnl k =
@@ -30,10 +30,10 @@ from_named fv_names t =
     from_named' bv_names (NamedTermVar x) =
       TermVar (v_idx bv_names x)
     from_named' bv_names (NamedTermLambda x t) =
-      TermLambda (from_named' (x :: bv_names) t)
+      TermLambda (from_named' (x : bv_names) t)
     from_named' bv_names (NamedTermApply tf tx) =
       TermApply (from_named' bv_names tf) (from_named' bv_names tx)
     v_idx bv_names x =
       case elemIndex x bv_names of
-        Just k -> k
-        None   -> just $ elemIndex x fv_names
+        Just k  -> k
+        Nothing -> (fromJust $ elemIndex x fv_names) + (length bv_names)
