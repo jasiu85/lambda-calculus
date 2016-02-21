26c8fd43954cb1492e4c9f37398fa13183fde33b
Michal Jaszczyk <jasiu85@gmail.com>
2016-02-21 22:53:31 +0000

    Read term from stdin.

--- main.hs
+++ main.hs
@@ -2,23 +2,25 @@ import Lambda.Syntax
 import Lambda.Analysis
 import Lambda.Named
 import Lambda.Show
+import Lambda.Read
 
-test_term =
-  (TermApply
-    (TermLambda (TermLambda
-      (TermApply
-        (TermApply (TermVar 2) (TermVar 1))
-        (TermApply (TermVar 3) (TermVar 0)))))
-    (TermVar 3))
+test_term :: NamedTerm
+test_term = read "\\x.x"
 
 fv_names = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 bv_names = ["x", "y", "z", "t", "u", "v", "w"]
-named_term = to_named fv_names bv_names test_term
+named_term = test_term
 unnamed_term = from_named fv_names named_term
 
 main = do
-  putStrLn ("Term:      " ++ (show test_term))
+  putStrLn "Enter term:"
+  str_term <- getLine
+  let named_term = read str_term
+  let unnamed_term = from_named fv_names named_term
+  let renamed_term = to_named fv_names bv_names unnamed_term
+  putStrLn ("Original:  " ++ str_term)
   putStrLn ("Named:     " ++ (show named_term))
   putStrLn ("Unnamed:   " ++ (show unnamed_term))
-  putStrLn ("Rank:      " ++ (show (rank test_term)))
-  putStrLn ("Free vars: " ++ (show (free_vars test_term)))
+  putStrLn ("Renamed:   " ++ (show renamed_term))
+  putStrLn ("Rank:      " ++ (show (rank unnamed_term)))
+  putStrLn ("Free vars: " ++ (show (free_vars unnamed_term)))
