8dca9a2a21e0a46265ffe8b9f17968d74030c27f
Michal Jaszczyk <jasiu85@gmail.com>
2016-02-21 22:53:15 +0000

    Print more parentheses in "show".

--- Lambda/Show.hs
+++ Lambda/Show.hs
@@ -7,14 +7,14 @@ instance Show Term where
   showsPrec n (TermVar k) =
     shows k
   showsPrec n (TermLambda t) =
-    showParen (n > 0) (showString "\\." . showsPrec 0 t)
+    showParen (n > 0) (showString "\\." . showsPrec 2 t)
   showsPrec n (TermApply tf tx) =
-    showParen (n > 1) (showsPrec 1 tf . showString " " . showsPrec 2 tx)
+    showParen (n > 1) (showsPrec 2 tf . showString " " . showsPrec 2 tx)
 
 instance Show NamedTerm where
   showsPrec n (NamedTermVar s) =
     showString s
   showsPrec n (NamedTermLambda x t) =
-    showParen (n > 0) (showString "\\" . showString x . showString "." . showsPrec 0 t)
+    showParen (n > 0) (showString "\\" . showString x . showString "." . showsPrec 2 t)
   showsPrec n (NamedTermApply tf tx) =
-    showParen (n > 1) (showsPrec 1 tf . showString " " . showsPrec 2 tx)
+    showParen (n > 1) (showsPrec 2 tf . showString " " . showsPrec 2 tx)
