dd264b413039ed762c20c3d28fa008a901aefde9
Michal Jaszczyk <jasiu85@gmail.com>
2016-02-22 00:43:17 +0000

    Update main.

--- main.hs
+++ main.hs
@@ -7,17 +7,12 @@ import Lambda.Read
 test_term :: NamedTerm
 test_term = read "\\x.x"
 
-fv_names = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
-bv_names = ["x", "y", "z", "t", "u", "v", "w"]
-named_term = test_term
-unnamed_term = from_named fv_names named_term
-
 main = do
   putStrLn "Enter term:"
   str_term <- getLine
   let named_term = read str_term
-  let unnamed_term = from_named fv_names named_term
-  let renamed_term = to_named fv_names bv_names unnamed_term
+  let unnamed_term = from_named named_term
+  let renamed_term = to_named unnamed_term
   putStrLn ("Original:  " ++ str_term)
   putStrLn ("Named:     " ++ (show named_term))
   putStrLn ("Unnamed:   " ++ (show unnamed_term))
