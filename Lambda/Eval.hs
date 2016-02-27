bf10d97b6c8d48d77cf397bd140e6a6d2dc3bf27
Michal Jaszczyk <jasiu85@gmail.com>
2016-02-27 17:05:08 +0000

    Better evaluators.

--- Lambda/Eval.hs
+++ Lambda/Eval.hs
@@ -1,21 +1,38 @@
 module Lambda.Eval where
 
-import Control.Applicative
+import Control.Monad.Reader
+import Control.Monad.Writer
 
 import Lambda.Syntax
 import Lambda.Manipulators
 
+report argTerm = do
+  contextTerm <- ask
+  tell $ [contextTerm argTerm]
+
 evalCallByName term = case term of
-  TermApply fun arg -> let fun' = evalCallByName fun in case fun' of
-    TermLambda x body -> reduce x body arg
-    otherwise -> TermApply fun' arg
-  otherwise -> term
+  TermApply fun arg -> do
+    fun' <- local (. \f -> TermApply f arg) (evalCallByName fun)
+    case fun' of
+      TermLambda x body -> do
+        let reduced = reduce x body arg
+        report reduced
+        evalCallByName reduced
+      otherwise -> do
+        return $ TermApply fun' arg
+  otherwise -> do
+    return $ term
 
 evalCallByValue term = case term of
-  TermApply fun arg -> let
-    fun' = evalCallByValue fun
-    arg' = evalCallByValue arg
-    in case fun' of
-      TermLambda x body -> reduce x body arg'
-      otherwise -> TermApply fun' arg'
-  otherwise -> term
+  TermApply fun arg -> do
+    fun' <- local (. \f -> TermApply f arg) (evalCallByValue fun)
+    arg' <- local (. \a -> TermApply fun' a) (evalCallByValue arg)
+    case fun' of
+      TermLambda x body -> do 
+        let reduced = reduce x body arg'
+        report reduced
+        evalCallByValue reduced
+      otherwise -> do
+        return $ TermApply fun' arg'
+  otherwise -> do
+    return $ term
