50a3bc7209fc0a9081929f66ce8005ab81b680ab
Michal Jaszczyk <jasiu85@gmail.com>
2016-03-06 19:33:17 +0000

    Implement denotational semantics, abandon single-step reporting.

--- Lambda/Eval.hs
+++ Lambda/Eval.hs
@@ -6,53 +6,45 @@ import Control.Monad.Writer
 import Lambda.Syntax
 import Lambda.Manipulators
 
-report argTerm = do
-  contextTerm <- ask
-  tell $ [contextTerm argTerm]
+evalCallByName term = eval term where
+  eval (TermApply fun arg) = term' where
+    fun' = eval fun
+    term' = eval $ reduce $ TermApply fun' arg
+  eval (TermMul left right) = term' where
+    left' = eval left
+    right' = eval right
+    term' = reduce $ TermMul left' right'
+  eval (TermAdd left right) = term' where
+    left' = eval left
+    right' = eval right
+    term' = reduce $ TermAdd left' right'
+  eval term = term
+  reduce (TermApply (TermLambda _ body) arg) =
+    betaReduce body arg
+  reduce (TermMul (TermConst left) (TermConst right)) =
+    TermConst (left * right)
+  reduce (TermAdd (TermConst left) (TermConst right)) =
+    TermConst (left + right)
+  reduce term = term
 
-evalCallByName term = case term of
-  TermApply fun arg -> do
-    fun' <- local (. \f -> TermApply f arg) (evalCallByName fun)
-    case fun' of
-      TermLambda x body -> do
-        let reduced = reduce x body arg
-        report reduced
-        evalCallByName reduced
-      otherwise -> do
-        return $ TermApply fun' arg
-  otherwise -> do
-    return $ term
-
-evalCallByValue term = case term of
-  TermApply fun arg -> do
-    fun' <- local (. \f -> TermApply f arg) (evalCallByValue fun)
-    arg' <- local (. \a -> TermApply fun' a) (evalCallByValue arg)
-    case fun' of
-      TermLambda x body -> do 
-        let reduced = reduce x body arg'
-        report reduced
-        evalCallByValue reduced
-      otherwise -> do
-        return $ TermApply fun' arg'
-  TermMul left right -> do
-    left' <- local (. \l -> TermMul l right) (evalCallByValue left)
-    right' <- local (. \r -> TermMul left' r) (evalCallByValue right)
-    case (left', right') of
-      (TermConst vl, TermConst vr) -> do
-        let reduced = TermConst (vl * vr)
-        report reduced
-        return reduced
-      otherwise -> do
-        return $ TermMul left' right'
-  TermAdd left right -> do
-    left' <- local (. \l -> TermAdd l right) (evalCallByValue left)
-    right' <- local (. \r -> TermAdd left' r) (evalCallByValue right)
-    case (left', right') of
-      (TermConst vl, TermConst vr) -> do
-        let reduced = TermConst (vl + vr)
-        report reduced
-        return reduced
-      otherwise -> do
-        return $ TermAdd left' right'
-  otherwise -> do
-    return $ term
+evalCallByValue term = eval term where
+  eval (TermApply fun arg) = term' where
+    fun' = eval fun
+    arg' = eval arg
+    term' = eval $ reduce $ TermApply fun' arg'
+  eval (TermMul left right) = term' where
+    left' = eval left
+    right' = eval right
+    term' = reduce $ TermMul left' right'
+  eval (TermAdd left right) = term' where
+    left' = eval left
+    right' = eval right
+    term' = reduce $ TermAdd left' right'
+  eval term = term
+  reduce (TermApply (TermLambda _ body) arg) =
+    betaReduce body arg
+  reduce (TermMul (TermConst left) (TermConst right)) =
+    TermConst (left * right)
+  reduce (TermAdd (TermConst left) (TermConst right)) =
+    TermConst (left + right)
+  reduce term = term
